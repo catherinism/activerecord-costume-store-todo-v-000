22eeafdf045ba362a99052ca4ab7acdfd7eb8dcd
Catherine Salazar <catcreates13@gmail.com>
2018-09-15 22:25:47 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
+      t.text :description
     end
   end
 end
