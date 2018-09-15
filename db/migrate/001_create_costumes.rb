921cf6dab6a1a2c8737a82306e69bfab8d9ea003
Catherine Salazar <catcreates13@gmail.com>
2018-09-15 22:02:22 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.text :image_url
       t.integer :size
-      t.timestamp
+      t.timestamp :created_at
 
     end
   end
