f5292fdbf89ab702a27929757cf59a241205cbe7
Catherine Salazar <catcreates13@gmail.com>
2018-09-15 22:23:59 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,11 +4,11 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
+      # t.string :theme
+      # t.integer :price
+      # t.boolean :family_friendly
+      # t.datetime :opening_time
+      # t.datetime :closing_time
     end
   end
 end
